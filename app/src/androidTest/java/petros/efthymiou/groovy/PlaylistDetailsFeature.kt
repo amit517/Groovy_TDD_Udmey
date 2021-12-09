7051866d9adc9cd4a29a16bc70c1f1c28d192018
Amit <amit@sharetrip.net>
2021-12-09 16:04:34 +0600

    112 lesson complete

--- app/src/androidTest/java/petros/efthymiou/groovy/PlaylistDetailsFeature.kt
+++ app/src/androidTest/java/petros/efthymiou/groovy/PlaylistDetailsFeature.kt
@@ -10,16 +10,21 @@ import org.junit.Test
 class PlaylistDetailsFeature : BaseUITest() {
 
     @Test
-    fun displaysPlaylistNameAndDetails(){
+    fun displaysPlaylistNameAndDetails() {
         Espresso.onView(
             CoreMatchers.allOf(
                 ViewMatchers.withId(R.id.playlist_image),
-                ViewMatchers.isDescendantOfA(nthChildOf(ViewMatchers.withId(R.id.playlist_list), 0))
+                ViewMatchers.isDescendantOfA(
+                    nthChildOf(
+                        ViewMatchers.withId(R.id.playlist_list),
+                        0
+                    )
+                )
             )
         ).perform(ViewActions.click())
 
         assertDisplayed("Hard Rock Cafe")
 
-        assertDisplayed("Rock your senses with this timeless signature vibe list. \\n\\n • Poison \\n • You shook me all night \\n • Zombie \\n • Rock'n Me \\n • Thunderstruck \\n • I Hate Myself for Loving you \\n • Crazy \\n • Knockin' on Heavens Door")
+        assertDisplayed("Rock your senses with this timeless signature vibe list. \n\n • Poison \n • You shook me all night \n • Zombie \n • Rock'n Me \n • Thunderstruck \n • I Hate Myself for Loving you \n • Crazy \n • Knockin' on Heavens Door")
     }
 }
--- app/src/main/java/petros/efthymiou/groovy/details/PlaylistDetailsService.kt
+++ app/src/main/java/petros/efthymiou/groovy/details/PlaylistDetailsService.kt
@@ -1,10 +1,20 @@
 package petros.efthymiou.groovy.details
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class PlaylistDetailsService @Inject constructor () {
+class PlaylistDetailsService @Inject constructor(
+    private val api: PlaylistDetailsApi
+) {
     suspend fun fetchPlaylistDetails(id: String): Flow<Result<PlaylistDetails>> {
-        TODO()
+        return flow { emit(Result.success(api.fetchPlaylistDetails(id))) }.catch {
+            emit(
+                Result.failure(
+                    RuntimeException("Something went wrong")
+                )
+            )
+        }
     }
 }
